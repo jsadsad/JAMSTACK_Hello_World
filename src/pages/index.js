e7f3f7e4ae9fa4450d584dd4c6c8216291d6582f
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-08-28 13:54:13 -0700

    slight decoration

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hello, it's me.</h1>
-    <p></p>
+    <h1 style={{color: 'green'}}>♪ Hello, it's me ♪</h1>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
